643bfffafe5c475ad9cb901b70a1c7ba1b944156
Ashish Kumar <89826676+ashish246kumar@users.noreply.github.com>
2024-03-23 16:38:51 +0530

    Create CompletableFuture.java

---
+++ CompletableFuture.java
@@ -0,0 +1,8 @@
+CompletableFuture API. It provides a way to 
+perform asynchronous programming in Java, enabling you to execute tasks concurrently and handle their results when they become available.
+In the context of CompletableFuture, callback functions are represented by methods like thenApply, thenAccept, thenRun.
+*************************************************************************************************
+CompletableFuture provides a more flexible and powerful API for working with asynchronous computations than Future. It offers non-blocking methods, composition methods, better
+exception handling, and explicit completion methods, which makes it easier to write robust and scalable concurrent code.
+
+  
